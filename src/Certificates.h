077d5f55309a85a4a45e45eabacb5f5ff81edef8
Q22012003 <tqui2024@gmail.com>
2026-03-10 08:40:03 -0700

    Thêm xe V3

--- src/Certificates.h
+++ src/Certificates.h
@@ -20,7 +20,7 @@ static const char* AWS_IOT_ENDPOINT = "a33b04z4mm5umj-ats.iot.us-east-1.amazonaw
 
 
 #ifndef VEHICLE_ID 
-  #define VEHICLE_ID 2
+  #define VEHICLE_ID 3
 #endif
 #if (VEHICLE_ID == 1)
   #define MQTT_CLIENT_ID        "ESP32_GM65_Client_01"
@@ -32,6 +32,12 @@ static const char* AWS_IOT_ENDPOINT = "a33b04z4mm5umj-ats.iot.us-east-1.amazonaw
   #define MQTT_CLIENT_ID        "ESP32_GM65_Client_02"
   #define MQTT_TOPIC_SUBSCRIBE  "car/V2/command"
   #define MQTT_TOPIC_PUBLISH    "car/V2/matrix_position"
+  #include "Certificates_V2.h"
+
+#elif (VEHICLE_ID == 3)
+  #define MQTT_CLIENT_ID        "ESP32_GM65_Client_03"
+  #define MQTT_TOPIC_SUBSCRIBE  "car/V3/command"
+  #define MQTT_TOPIC_PUBLISH    "car/V3/matrix_position"
   #include "Certificates_V2.h"
 
 #else
